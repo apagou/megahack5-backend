c0fadeb76ebe3e03c16afb6f7a2707a15a38344b
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2020-11-14 15:30:53 -0300

    fix: change type of column cvv_hash

--- src/models/CreditCard.js
+++ src/models/CreditCard.js
@@ -1,67 +1,65 @@
 import Sequelize, { Model } from 'sequelize';
-import appConfig from '../config/appConfig'
 import bcryptjs from 'bcryptjs';
+import appConfig from '../config/appConfig';
 
 export default class CreditCard extends Model {
-    static init(sequelize) {
-        super.init(
-            {
-                card_number: {
-                    type: Sequelize.VIRTUAL,
-                    defaultValue: '',
-                },
-                CVV_hash:{
-                    type: Sequelize.INTEGER,
-                    defaultValue: ''
-                },
-                card_number_hash: {
-                    type: Sequelize.STRING,
-                    defaultValue: '',
-                },
-                last_digits: {
-                    type: Sequelize.STRING,
-                    defaultValue: '',
-                },
-                CVV: {
-                    type: Sequelize.VIRTUAL,
-                    defaultValue: '',
-                },
-                validity_date: {
-                    type: Sequelize.STRING,
-                    defaultValue: '',
-                },
-                user_id: {
-                    type: Sequelize.INTEGER,
-                    defaultValue: '',
-                },
-                owner_name: {
-                    type: Sequelize.STRING,
-                    defaultValue: '',
-                },
-                CPF: {
-                    type: Sequelize.STRING,
-                    defaultValue: '',
-                },
-            },
-            {
-                sequelize,
-            },
-        );
+  static init(sequelize) {
+    super.init(
+      {
+        card_number: {
+          type: Sequelize.VIRTUAL,
+          defaultValue: '',
+        },
+        CVV_hash: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+        },
+        card_number_hash: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+        },
+        last_digits: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+        },
+        CVV: {
+          type: Sequelize.VIRTUAL,
+          defaultValue: '',
+        },
+        validity_date: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+        },
+        user_id: {
+          type: Sequelize.INTEGER,
+          defaultValue: '',
+        },
+        owner_name: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+        },
+        CPF: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+        },
+      },
+      {
+        sequelize,
+      },
+    );
 
+    this.addHook('beforeSave', async (creditCard) => {
+      if (creditCard.card_number) { creditCard.card_number_hash = await bcryptjs.hash(creditCard.card_number, 8); }
+      if (creditCard.CVV) { creditCard.CVV_hash = await bcryptjs.hash((creditCard.CVV).toString(), 8); }
+    });
+    return this;
+  }
 
-        this.addHook('beforeSave', async (creditCard) => {
-            if (creditCard.card_number) { creditCard.card_number_hash = await bcryptjs.hash(creditCard.card_number, 8); }
-            if (creditCard.CVV) { creditCard.CVV_hash = await bcryptjs.hash((creditCard.CVV).toString(), 8); }
-        });
-        return this;
-    }
+  CreditCardIsValid(creditCard) {
+    return bcryptjs.compare(creditCard, this.card_number_hash);
+  }
 
-    CreditCardIsValid(creditCard) {
-        return bcryptjs.compare(creditCard, this.card_number_hash);
-    }
-
-    static associate(models) {
-        this.belongsTo(models.User, { foreignKey: 'user_id' });
-    }
-
+  static associate(models) {
+    this.belongsTo(models.User, { foreignKey: 'user_id' });
+  }
 }
