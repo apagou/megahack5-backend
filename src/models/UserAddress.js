3af21a296b386edb5972738035cece7564da0c1b
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2020-11-14 00:53:08 -0300

    [feat]: add latitude and longitude on UserAddress model

--- src/models/UserAddress.js
+++ src/models/UserAddress.js
@@ -1,26 +1,34 @@
 import Sequelize, { Model } from 'sequelize';
 
 export default class UserAddress extends Model {
-    static init(sequelize) {
-        super.init(
-            {
-                address: {
-                    type: Sequelize.STRING,
-                    defaultValue: '',
-                },
-                number: {
-                    type: Sequelize.STRING,
-                    defaultValue: '',
-                },
-                user_id: {
-                    type: Sequelize.INTEGER,
-                    defaultValue: 0,
-                },
-            },
-            {
-                sequelize,
-            },
-        );
-        return this;
-    }
+  static init(sequelize) {
+    super.init(
+      {
+        address: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+        },
+        latitude: {
+          type: Sequelize.DECIMAL(18, 8),
+          defaultValue: '',
+        },
+        longitude: {
+          type: Sequelize.DECIMAL(18, 8),
+          defaultValue: '',
+        },
+        user_id: {
+          type: Sequelize.INTEGER,
+          defaultValue: 0,
+        },
+      },
+      {
+        sequelize,
+      },
+    );
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.Order, { foreignKey: 'address' });
+  }
 }
