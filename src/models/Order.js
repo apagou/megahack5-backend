5c724664b90bf41d290a7216c766428b8e3c6098
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2020-11-14 00:52:41 -0300

    [feat]: add address column on Order model

--- src/models/Order.js
+++ src/models/Order.js
@@ -1,39 +1,40 @@
 import Sequelize, { Model } from 'sequelize';
 
-
 export default class Order extends Model {
-    static init(sequelize) {
-        super.init(
-            {
-                shop_id: {
-                    type: Sequelize.STRING,
-                    defaultValue: '',
-                },
-                status:{
-                    type: Sequelize.STRING,
-                    defaultValue: '',
-                },
-                user_id: {
-                    type: Sequelize.INTEGER,
-                    defaultValue: '',
-                },
-                freight: {
-                    type: Sequelize.DECIMAL(18,2),
-                    defaultValue: '',
-                },
-            },
-            {
-                sequelize,
-            },
-        );
-        return this;
-    }
+  static init(sequelize) {
+    super.init(
+      {
+        shop_id: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+        },
+        status: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+        },
+        user_id: {
+          type: Sequelize.INTEGER,
+          defaultValue: '',
+        },
+        freight: {
+          type: Sequelize.DECIMAL(18, 2),
+          defaultValue: '',
+        },
+        address: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+        },
+      },
+      {
+        sequelize,
+      },
+    );
+    return this;
+  }
 
-    static associate(models){
-        this.hasMany(models.OrderedProducts, {foreignKey: 'order_id'})
-        this.belongsTo(models.Shop, {foreignKey: 'shop_id'})
-    }
-
-
-
+  static associate(models) {
+    this.hasMany(models.OrderedProducts, { foreignKey: 'order_id' });
+    this.hasOne(models.UserAddress, { foreignKey: 'address' });
+    this.belongsTo(models.Shop, { foreignKey: 'shop_id' });
+  }
 }
